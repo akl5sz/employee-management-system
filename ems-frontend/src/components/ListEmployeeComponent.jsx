cbb40451150d7bd21b5d100180b31057cfc86ce2
Angie <akl5sz@virginia.edu>
2025-08-27 13:02:18 -0400

    created a ListEmployeeComponent in React with dummy data to display the list of employees

---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+    const dummyData = [
+        {
+            "id": 1,
+            "firstName": "Mary",
+            "lastName": "M",
+            "email":"mary@gmail.com"
+        },
+        {
+            "id": 2,
+            "firstName": "John",
+            "lastName": "Doe",
+            "email":"john@gmail.com"
+        },
+        {
+            "id": 3,
+            "firstName": "Jane",
+            "lastName": "Doe",
+            "email":"jane@gmail.com"
+        }
+    ]
+  
+  
+    return (
+    <div>
+        <h2>List of Employees</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee ID</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee => 
+                         <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+
+}
+
+export default ListEmployeeComponent
