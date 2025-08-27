aec569a254fb9c5cd410821040e2ed0204b59a9f
Angie <akl5sz@virginia.edu>
2025-08-27 16:49:09 -0400

    added header component

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HeaderComponent from './components/HeaderComponent.jsx';
 import ListEmployeeComponent from './components/ListEmployeeComponent.jsx';
 
 function App() {
 
   return (
     <>
+        <HeaderComponent></HeaderComponent>
         <ListEmployeeComponent></ListEmployeeComponent>
     </>
   )
