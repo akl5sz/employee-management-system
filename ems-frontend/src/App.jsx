98f98ec3e0c465c36d94f33cb46116f9052fcaa0
Angie <akl5sz@virginia.edu>
2025-08-27 17:09:33 -0400

    basic configuration of routes for react components using react-router-dom lib

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,14 +2,22 @@ import './App.css'
 import FooterComponent from './components/FooterComponent.jsx';
 import HeaderComponent from './components/HeaderComponent.jsx';
 import ListEmployeeComponent from './components/ListEmployeeComponent.jsx';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
         <HeaderComponent></HeaderComponent>
-        <ListEmployeeComponent></ListEmployeeComponent>
+        <Routes>
+          {/* //localhost:3000 */}
+          <Route path='/' element={<ListEmployeeComponent></ListEmployeeComponent>}></Route>
+          {/* //localhost:3000/employees */}
+          <Route path='/employees' element={<ListEmployeeComponent></ListEmployeeComponent>}></Route>
+        </Routes>
         <FooterComponent></FooterComponent>
+      </BrowserRouter>
     </>
   )
 }
